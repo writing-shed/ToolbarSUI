011b583f80a7e570bbeb4756d1779f60bf127ae8
Keith Lander <keithlander@me.com>
2020-10-17 10:31:53 +0100

    YAN change to Padding

--- Sources/ToolbarSUI/ToolbarView.swift
+++ Sources/ToolbarSUI/ToolbarView.swift
@@ -51,7 +51,7 @@ public struct ToolbarView : View {
                 }
             }
         }
-        .padding(EdgeInsets(top: 3, leading: 0, bottom: 3, trailing: 0))
+        .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
         .modifier(RectangleOverlay())
     }
 }
